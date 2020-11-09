5c6da922bbfbbf759b30e9894dec55266d3898d7
VSS Karthik(linux) <vsskarthik8@gmail.com>
2020-11-09 17:49:35 +0530

    Bug Fixes in parser

--- bin/parser.py
+++ bin/parser.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import yaml
+
 
 def yaml_handler(yaml_file,key,value):
     f = open(yaml_file,'r')
@@ -14,9 +14,11 @@ def yaml_handler(yaml_file,key,value):
 #[TODO] Add Log Function
 
 
-def run_sim(yaml_file,rates):
-    yaml_handler(yaml_file,'packet_injection_rate',rates[0])
-    os.system(f'./noxim -config {yaml_file} > ./test_results/temp_res')
+def run_sim(yaml_file,rate,routing_algo,selection_algo):
+    yaml_handler(yaml_file,'packet_injection_rate',rate)
+    yaml_handler('temp.yaml','routing_algorithm',routing_algo)
+    yaml_handler('temp.yaml','selection_strategy',selection_algo)
+    os.system(f'./noxim -config ./temp.yaml > ./test_results/temp_res')
     
     
 def parse(filename):
@@ -31,7 +33,13 @@ def parse(filename):
 
 if __name__ == '__main__':
     config_file = '../config_examples/my_config.yaml'
-    run_sim(config_file,[0.01])
-    print(parse('./test_results/temp_res'))
+    injection_rates = [0.01,0.02,0.1]
+    routing_algo = 'ODD_EVEN'
+    selection_algo = 'RANDOM'
+    print(f'{routing_algo} | {selection_algo}')
+    for i in injection_rates:
+        run_sim(config_file,i,routing_algo,selection_algo)
+        latency = parse('./test_results/temp_res')
+        print(f'{i} : {latency}')
 
 
